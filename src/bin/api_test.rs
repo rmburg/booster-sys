c2df0bfdf2747a51ae1726d81ff9d7cf84d8e0b1
Rasmus Mecklenburg <42248344+rmburg@users.noreply.github.com>
2025-08-28 17:34:46 +0200

    Update api_test binary

--- src/bin/api_test.rs
+++ src/bin/api_test.rs
@@ -1,15 +1,14 @@
 use cxx::let_cxx_string;
 
-use booster_sys::robot::{b1::ffi::{b1_loco_client_new, LocoApiId}, ffi::init_channel_factory};
+use booster_sys::robot::{
+    b1::ffi::b1_loco_client_new, common::ffi::RobotMode, ffi::init_channel_factory,
+};
 
 fn main() {
     let_cxx_string!(network_interface = "127.0.0.1");
     init_channel_factory(&network_interface);
-    
-    let api_id = LocoApiId::kWaveHand;
-    let_cxx_string!(param = "");
+
     let mut client = b1_loco_client_new();
 
-    client.pin_mut().Init();
-    client.pin_mut().SendApiRequest(api_id, &param);
+    client.pin_mut().ChangeMode(RobotMode::kDamping);
 }
